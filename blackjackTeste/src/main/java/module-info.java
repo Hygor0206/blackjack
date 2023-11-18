00c74ef4e5709334ab06427a23379eac3aff9f92
Hygor Marques <68515062+Hygor0206@users.noreply.github.com>
2023-11-18 03:06:29 -0300

    Removing dump files

--- blackjackTeste/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ifc.blackjack {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    
-    opens ifc.blackjack to javafx.fxml;
-    exports ifc.blackjack;
-}
